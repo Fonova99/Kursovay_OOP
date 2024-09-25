c83622fb9fb8fc6e4fce16f60fbb6b6a5d2848cb
Arina Fonova <fonova99@list.ru>
2024-09-25 18:41:07 +0300

    Exceptions

--- src/main/java/TextException.java
+++ src/main/java/TextException.java
@@ -8,6 +8,7 @@ public class TextException extends  Exception{
         String str = disease.getText();
         if (str.contains("Введите название заболевания")) throw new TextException();
         if (str.length() == 0) throw new NullPointerException();
+        
     }
 }
 
