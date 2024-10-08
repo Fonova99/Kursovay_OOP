b5400e33a152fb5cb3996c61f6fc917a6f0da816
Arina Fonova <fonova99@list.ru>
2024-10-09 00:57:57 +0300

    Creating xmlfile

--- src/main/java/XmlFile.java
+++ src/main/java/XmlFile.java
@@ -1,132 +1,240 @@
 import org.w3c.dom.*;
-import org.xml.sax.SAXException;
-
+import javax.xml.parsers.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import java.awt.*;
+import java.io.*;
+import org.xml.sax.SAXException;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.awt.*;
-import java.io.*;
-
 
 public class XmlFile {
-    Document doc;
-    Object monitor = new Object();
+
+    private final Object monitor = new Object();
+    private Document doc;
     DatabaseManager db = new DatabaseManager();
 
-    public void recordXmlFile(DefaultTableModel model) {
+    // Метод для чтения данных пациентов из XML-файла
+    public void readXmlFilePatients(DefaultTableModel modelPatients, DefaultTableModel modelDoctors) {
         synchronized (monitor) {
-            if (model == null) {
-                System.err.println("Model is not initialized.");
-                return;
-            }
-            FileDialog read = new FileDialog((Frame) null, "Запись данных", FileDialog.SAVE);
-            read.setFile("*.xml");
-            read.setVisible(true); // Отобразить запрос пользователю
-            String fileName = read.getFile();
-            if (fileName == null) {
-                return;
-            }
-            fileName = read.getDirectory() + read.getFile(); // Определить имя выбранного каталога и файла
-            try {
-                DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder(); // Создание парсера документа
-                Document doc = builder.newDocument(); // Создание пустого документа
-
-                Node administration = doc.createElement("Administration"); // Создание корневого элемента
-                doc.appendChild(administration); // Добавление его в документ
-
-                // Создание дочерних элементов и присвоение значений атрибутам
-                for (int i = 0; i < model.getRowCount(); i++) {
-                    Element visit = doc.createElement("visit");
-                    administration.appendChild(visit);
-                    visit.setAttribute("doctor", (String) model.getValueAt(i, 0));
-                    visit.setAttribute("speciality", (String) model.getValueAt(i, 1));
-                    visit.setAttribute("cabinet", (String) model.getValueAt(i, 2));
-                    visit.setAttribute("workSchedule", (String) model.getValueAt(i, 3));
-                    visit.setAttribute("patient", (String) model.getValueAt(i, 4));
-                    visit.setAttribute("disease", (String) model.getValueAt(i, 5));
-                }
-
-                // Преобразуем XML-документ в строку для редактирования
-                Transformer transformer = TransformerFactory.newInstance().newTransformer(); // Создание преобразователя документа
-                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-                StringWriter writer = new StringWriter();
-                transformer.transform(new DOMSource(doc), new StreamResult(writer));
-                String xmlString = writer.toString();
-
-                // Показываем окно редактирования
-                String editedXml = showEditor(xmlString);
-
-                // Преобразуем отредактированную строку обратно в XML
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder newBuilder = factory.newDocumentBuilder();
-                Document editedDoc = newBuilder.parse(new ByteArrayInputStream(editedXml.getBytes()));
-
-                // Сохраняем отредактированный XML в файл
-                Transformer trans = TransformerFactory.newInstance().newTransformer();
-                FileWriter fw = new FileWriter(fileName);
-                trans.transform(new DOMSource(editedDoc), new StreamResult(fw));
-
-            } catch (ParserConfigurationException | TransformerException | IOException | SAXException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    private String showEditor(String xmlString) {
-        JTextArea textArea = new JTextArea(xmlString);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setPreferredSize(new Dimension(1000, 400));
-
-        int result = JOptionPane.showConfirmDialog(null, scrollPane, "Редактирование XML", JOptionPane.OK_CANCEL_OPTION);
-        if (result == JOptionPane.OK_OPTION) {
-            return textArea.getText();
-        } else {
-            return xmlString; // Если пользователь отменил редактирование, возвращаем исходный XML
-        }
-    }
-
-    public void readXmlFile(DefaultTableModel model) {
-        synchronized (monitor) {
-            if (model == null) {
+            if (modelPatients == null) {
                 System.err.println("Model is not initialized.");
                 return;
             }
             FileDialog read = new FileDialog((Frame) null, "Чтение данных", FileDialog.LOAD);
             read.setFile("*.xml");
-            read.setVisible(true); // Отобразить запрос пользователю
-            String fileName = read.getDirectory() + read.getFile(); // Определить имя выбранного каталога и файла
+            read.setVisible(true);
+            String fileName = read.getDirectory() + read.getFile();
+            if (fileName == null || fileName.isEmpty()) {
+                System.err.println("File name is not specified.");
+                return;
+            }
 
             try {
-                DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder(); //Создание парсера документа
-                doc = builder.parse(new File(fileName)); //Чтение из файла
+                DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                doc = builder.parse(new File(fileName));
                 doc.getDocumentElement().normalize();
-            } catch (ParserConfigurationException e) { // Обработка ошибки парсера при чтении данных из XML-файла
+            } catch (ParserConfigurationException | SAXException | IOException e) {
                 e.printStackTrace();
-            } catch (SAXException | IOException e) {
+                return;
+            }
+
+            NodeList visits = doc.getElementsByTagName("visit");
+            modelPatients.setRowCount(0); // Очистка таблицы
+
+            for (int i = 0; i < visits.getLength(); i++) {
+                Node elem = visits.item(i);
+                NamedNodeMap attrs = elem.getAttributes();
+                String doctor = attrs.getNamedItem("doctor").getNodeValue();
+                String patient = attrs.getNamedItem("patient").getNodeValue();
+                String disease = attrs.getNamedItem("disease").getNodeValue();
+
+                if (checkDoctorExists(doctor, modelDoctors)) {
+                    modelPatients.addRow(new String[]{doctor, patient, disease});
+                } else {
+                    JOptionPane.showMessageDialog(null, "Врач " + doctor + " не найден в списке врачей.", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                }
+
+                db.updatePatients(modelPatients);
+            }
+        }
+    }
+
+    // Метод для записи данных пациентов в XML-файл
+    public void recordXmlFilePatients(DefaultTableModel modelPatients) {
+        synchronized (monitor) {
+            if (modelPatients == null) {
+                System.err.println("Model is not initialized.");
+                return;
+            }
+            FileDialog save = new FileDialog((Frame) null, "Запись данных", FileDialog.SAVE);
+            save.setFile("*.xml");
+            save.setVisible(true);
+            String fileName = save.getDirectory() + save.getFile();
+            if (fileName == null || fileName.isEmpty()) {
+                return;
+            }
+
+            try {
+                DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                Document doc = builder.newDocument();
+                Node administration = doc.createElement("Administration");
+                doc.appendChild(administration);
+
+                for (int i = 0; i < modelPatients.getRowCount(); i++) {
+                    Element visit = doc.createElement("visit");
+                    administration.appendChild(visit);
+                    visit.setAttribute("doctor", (String) modelPatients.getValueAt(i, 0));
+                    visit.setAttribute("patient", (String) modelPatients.getValueAt(i, 1));
+                    visit.setAttribute("disease", (String) modelPatients.getValueAt(i, 2));
+                }
+
+                Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+                StringWriter writer = new StringWriter();
+                transformer.transform(new DOMSource(doc), new StreamResult(writer));
+                String xmlString = writer.toString();
+
+                String editedXml = showEditor(xmlString);
+                Document editedDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new ByteArrayInputStream(editedXml.getBytes()));
+
+                Transformer trans = TransformerFactory.newInstance().newTransformer();
+                FileWriter fw = new FileWriter(fileName);
+                trans.transform(new DOMSource(editedDoc), new StreamResult(fw));
+            } catch (ParserConfigurationException | TransformerException | IOException | SAXException e) {
                 e.printStackTrace();
             }
-            NodeList visits = doc.getElementsByTagName("visit"); //Получение списка элементов с именем visit
-            model.setRowCount(0); //Очистка таблицы
-            for (int i = 0; i < visits.getLength(); i++) { //Цикл просмотра списка элементов и запись данных в таблицу
-                Node elem = visits.item(i); //Выбор очередного элемента списка
-                NamedNodeMap attrs = elem.getAttributes(); //Получение списка атрибутов элемента
-                //Чтение атрибутов элемента
+        }
+    }
+
+    // Метод для чтения данных врачей из XML-файла
+    public void readXmlFileDoctors(DefaultTableModel modelDoctors) {
+        synchronized (monitor) {
+            if (modelDoctors == null) {
+                System.err.println("Model is not initialized.");
+                return;
+            }
+            FileDialog read = new FileDialog((Frame) null, "Чтение данных", FileDialog.LOAD);
+            read.setFile("*.xml");
+            read.setVisible(true);
+            String fileName = read.getDirectory() + read.getFile();
+            if (fileName == null || fileName.isEmpty()) {
+                System.err.println("File name is not specified.");
+                return;
+            }
+
+            try {
+                DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                doc = builder.parse(new File(fileName));
+                doc.getDocumentElement().normalize();
+            } catch (ParserConfigurationException | SAXException | IOException e) {
+                e.printStackTrace();
+                return;
+            }
+
+            NodeList visits = doc.getElementsByTagName("visit");
+            modelDoctors.setRowCount(0); // Очистка таблицы
+
+            for (int i = 0; i < visits.getLength(); i++) {
+                Node elem = visits.item(i);
+                NamedNodeMap attrs = elem.getAttributes();
                 String doctor = attrs.getNamedItem("doctor").getNodeValue();
                 String speciality = attrs.getNamedItem("speciality").getNodeValue();
                 String cabinet = attrs.getNamedItem("cabinet").getNodeValue();
                 String workSchedule = attrs.getNamedItem("workSchedule").getNodeValue();
-                String patient = attrs.getNamedItem("patient").getNodeValue();
-                String disease = attrs.getNamedItem("disease").getNodeValue();
-                model.addRow(new String[]{doctor, speciality, cabinet, workSchedule, patient, disease}); //Запись данных в таблицу
-                db.updateDatabase(model);
+                modelDoctors.addRow(new String[]{doctor, speciality, cabinet, workSchedule});
+                db.updateDoctors(modelDoctors);
             }
         }
     }
+
+    // Метод для записи данных врачей в XML-файл
+    public void recordXmlFileDoctors(DefaultTableModel modelDoctors) {
+        synchronized (monitor) {
+            if (modelDoctors == null) {
+                System.err.println("Model is not initialized.");
+                return;
+            }
+            FileDialog save = new FileDialog((Frame) null, "Запись данных", FileDialog.SAVE);
+            save.setFile("*.xml");
+            save.setVisible(true);
+            String fileName = save.getDirectory() + save.getFile();
+            if (fileName == null || fileName.isEmpty()) {
+                return;
+            }
+
+            try {
+                DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                Document doc = builder.newDocument();
+                Node administration = doc.createElement("Administration");
+                doc.appendChild(administration);
+
+                for (int i = 0; i < modelDoctors.getRowCount(); i++) {
+                    Element visit = doc.createElement("visit");
+                    administration.appendChild(visit);
+                    visit.setAttribute("doctor", (String) modelDoctors.getValueAt(i, 0));
+                    visit.setAttribute("speciality", (String) modelDoctors.getValueAt(i, 1));
+                    visit.setAttribute("cabinet", (String) modelDoctors.getValueAt(i, 2));
+                    visit.setAttribute("workSchedule", (String) modelDoctors.getValueAt(i, 3));
+                }
+
+                Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+                StringWriter writer = new StringWriter();
+                transformer.transform(new DOMSource(doc), new StreamResult(writer));
+                String xmlString = writer.toString();
+
+                String editedXml = showEditor(xmlString);
+                Document editedDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new ByteArrayInputStream(editedXml.getBytes()));
+
+                Transformer trans = TransformerFactory.newInstance().newTransformer();
+                FileWriter fw = new FileWriter(fileName);
+                trans.transform(new DOMSource(editedDoc), new StreamResult(fw));
+            } catch (ParserConfigurationException | TransformerException | IOException | SAXException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Проверка наличия врача в модели врачей
+    private boolean checkDoctorExists(String doctorName, DefaultTableModel modelDoctors) {
+        for (int i = 0; i < modelDoctors.getRowCount(); i++) {
+            String existingDoctor = (String) modelDoctors.getValueAt(i, 0);
+            if (existingDoctor.equals(doctorName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Очистка таблиц пациентов и врачей
+    public void clearTableModel(DefaultTableModel model) {
+        synchronized (monitor) {
+            if (model != null) {
+                model.setRowCount(0);  // Удалить все строки из таблицы
+            }
+        }
+    }
+
+    // Метод для редактирования XML
+    private String showEditor(String text) {
+        JTextArea textArea = new JTextArea(text);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(800, 400));
+
+        int result = JOptionPane.showConfirmDialog(null, scrollPane, "Редактирование XML", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+        if (result == JOptionPane.OK_OPTION) {
+            return textArea.getText(); // Возвращаем отредактированный текст
+        } else {
+            return text; // Если пользователь нажал "Отмена", возвращаем исходный текст
+        }
+    }
+
+
 }
